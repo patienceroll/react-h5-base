2e8c2aa0447904d442a910a3dd4a73049cffd2f8
zhangxianlei <1079105171@qq.com>
2023-10-18 11:00:51 +0800

    develop

--- src/components/error-element/index.tsx
+++ src/components/error-element/index.tsx
@@ -1,9 +1,11 @@
 import { Empty } from "react-vant/es/empty";
 
+import './index.less'
+
 export default function () {
   return (
-    <div style={{ height: "100vh", width: "100vw", textAlign: "center" }}>
-      <Empty image="error" description="加载页面失败,请联系管理员" />
+    <div className="components-error-element" style={{ height: "100vh", width: "100vw", textAlign: "center" }}>
+      <Empty image="error" description={<div className="components-error-element-tip">加载配置失败,请联系管理员</div>} />
     </div>
   );
 }
