636133614bd7b490ea39253a70c15dcfb003f7af
zhangxianlei <1079105171@qq.com>
2024-08-20 09:32:13 +0800

    doc

--- src/pages/leave-words/index.tsx
+++ src/pages/leave-words/index.tsx
@@ -46,7 +46,7 @@ export default function () {
       className="page-leave-words"
       style={{ backgroundImage: `url(${assets.p8})` }}
     >
-      <Swiper vertical autoplay loop style={{ height: "100%" }}>
+      <Swiper vertical autoplay={10000} loop style={{ height: 3125 }}>
         {renderData.map((items) => (
           <Swiper.Item>
             {items.map((item) => (
